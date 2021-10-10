8089a9e61840976077f839da71c11921dfb243ae
courtuni <z5175357@ad.unsw.edu.au>
2021-10-10 16:42:28 +1100

    Laser Main initial code for reading data
    Builds successfully from home machine.

--- Laser/LaserMain.cpp
+++ Laser/LaserMain.cpp
@@ -9,8 +9,45 @@ using namespace System;
 using namespace System::Diagnostics;
 using namespace System::Threading;
 
+using namespace System::Net::Sockets;
+using namespace System::Net;
+using namespace System::Text;
+
 int main()
 {
+	int PortNumber = 2111;
+
+	TcpClient^ Client;
+
+	array<unsigned char>^ SendData;
+	array<unsigned char>^ ReadData;
+
+
+	String^ AskScan = gcnew String("sRN LMDscandata");
+	String^ ResponseData;
+
+	Client = gcnew TcpClient("192.168.1.200", PortNumber);
+
+	Client->NoDelay = true;
+	Client->ReceiveTimeout = 500; //ms
+	Client->SendTimeout = 500; //ms
+	Client->ReceiveBufferSize = 1024;
+	Client->SendBufferSize = 1024;
+
+
+	SendData = gcnew array<unsigned char>(16);
+	ReadData = gcnew array<unsigned char>(2500);
+
+	SendData = System::Text::Encoding::ASCII->GetBytes(AskScan);
+
+	NetworkStream^ Stream = Client->GetStream();
+
+	//while () {
+
+	//}
+
+
+
 	//Declaration
 	SMObject PMObj(TEXT("ProcessManagement"), sizeof(ProcessManagement));
 	//SM Creation and seeking access
@@ -29,12 +66,27 @@ int main()
 		//Sleep(2000);
 		QueryPerformanceCounter((LARGE_INTEGER*)&Counter);
 		TimeStamp = (double)Counter / (double)Frequency * 1000; // ms
-		Console::WriteLine("Laser time stamp    : {0,12:F3} {1,12:X2}", TimeStamp, Shutdown);
-		Thread::Sleep(25);
+		//Console::WriteLine("Laser time stamp    : {0,12:F3} {1,12:X2}", TimeStamp, Shutdown);
+		//Thread::Sleep(25);
+
+		Stream->WriteByte(0x02);
+		Stream->Write(SendData, 0, SendData->Length);
+		Stream->WriteByte(0x03);
+
+		System::Threading::Thread::Sleep(10);
+
+		Stream->Read(ReadData, 0, ReadData->Length);
+		ResponseData = System::Text::Encoding::ASCII->GetString(ReadData);
+		Console::WriteLine(ResponseData);
+
+
 		if (PMData->Shutdown.Status)
 			break;
 		if (_kbhit())
 			break;
 	}
+
+	Stream->Close();
+	Client->Close();
 	return 0;
 }
